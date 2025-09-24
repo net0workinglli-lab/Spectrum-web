088395fd5b0f38b1ae47a1134fa6c633521a487b
Sang2522 <netzeroworkinglli@gmail.com>
2025-09-24 10:42:57 +0700

    Thêm smooth scrolling và cải thiện Hero Section animation
    - Thêm smooth scrolling cho toàn bộ website
    - Cải thiện Hero Section với fade effect mượt mà
    - Thêm ScrollToTop button với animation
    - Tùy chỉnh scrollbar styling
    - Bỏ các hiệu ứng hover không cần thiết
    - Thêm useSmoothScroll hook cho programmatic scrolling

---
+++ out/_next/static/chunks/app/_not-found/page-fcbaf9b7d0694f3b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[492],{49891:(_,n,u)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_not-found/page",function(){return u(79520)}])}},_=>{_.O(0,[96,76,358],()=>_(_.s=49891)),_N_E=_.O()}]);
---
+++ out/_next/static/chunks/main-3b15340a9c46d752.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[792],{},_=>{_.O(0,[96,76],()=>_(_.s=46175)),_N_E=_.O()}]);
